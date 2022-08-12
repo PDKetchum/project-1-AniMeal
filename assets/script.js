570bc696d2a3eaaa27c348c8b798901ed6f3b408
PDKetchum <pxiong265@gmail.com>
2022-08-11 22:16:48 -0500

    added pseudo coding for next functions

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,11 @@ var $cardsBody = $("#cards-body");
 var anime;
 
 // Create a function that will fetch the API data from Jikan
-
+// create a function that will display the search result
+// Included in the search card:
+// Image of Anime poster data - images - large image url
+// sypnoisis
+// title
 function searchAnime() {
   var url = "https://api.jikan.moe/v4/anime?q=" + anime;
 
@@ -18,7 +22,6 @@ function searchAnime() {
       var title = data.data[0].title_english;
       var synopsis = data.data[1].synopsis;
       var poster = data.data[1].images.jpg.large_image_url;
-      
 
       var imageEl = $("<img>");
       var titleEl = $("<h1>");
@@ -26,31 +29,30 @@ function searchAnime() {
 
       titleEl.text(title);
       synopsisEl.text(synopsis);
-      imageEl.attr("src", poster)
+      imageEl.attr("src", poster);
 
       $cardsBody.append(imageEl, titleEl, synopsisEl);
-
     });
 }
 
 searchAnime();
 
-addEventListener('click')
-
-// Create a function that displays 6 anime suggestions
-
-// Included in the suggestion card:
-// Image of Anime poster data - images - large image url
-// Title
-
-// create a function that will display the search result
-// Included in the suggestion card:
-// Image of Anime poster data - images - large image url
-// sypnoisis
-// title
+addEventListener("click");
 
 $searchButton.on("click", printSearch);
-function printSearch(){
-  var userInput = $("#user-input")
-  anime.text(userInput) 
+function printSearch() {
+  var userInput = $("#user-input");
+  anime.text(userInput);
 }
+
+// Create a function that displays 6 anime suggestions
+// Fetch from the Jikan API random
+// create a for loop that will grab 6 anime suggestions
+// in the for loop, create elements for a card for the anime info to append to
+// in the for loop append the anime card to the html body
+
+// create a API fetch function for recipies
+// have the recipies randomized
+// target the data info needed
+// create elements for the recipie data to append to
+// append the recipe to html body
